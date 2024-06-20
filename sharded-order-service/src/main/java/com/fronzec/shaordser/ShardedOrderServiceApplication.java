4de32d93f823d9945720ce00c805c37c590b7ab7
Eduardo Flores <efloresambrosio@gmail.com>
2024-06-20 02:26:15 -0600

    New initial code for order service to use shardingsphere

---
+++ sharded-order-service/src/main/java/com/fronzec/shaordser/ShardedOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fronzec.shaordser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardedOrderServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShardedOrderServiceApplication.class, args);
+  }
+
+}
---
+++ sharded-order-service/src/test/java/com/fronzec/shaordser/ShardedOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fronzec.shaordser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardedOrderServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
